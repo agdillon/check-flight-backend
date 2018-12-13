48ca55d568a6bccc21f6f3e5cd84fdb165371102
Amanda Dillon <agdillon@gmail.com>
2018-12-13 12:58:35 -0700

    get rid of username column, merge examiners table into users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :firstName, :lastName, presence: true
-  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})\z/ }
   # before we uncomment this, make sure we're removing non-number characters from phone number on client side
   # validates :phone, format: { with: /^\d*$/ }, length: { is: 10 } should it be optional?
--- db/migrate/20181212232218_create_users.rb
+++ db/migrate/20181212232218_create_users.rb
@@ -1,12 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :username
       t.string :email
       t.string :phone
       t.string :password # not sure if we need this since we will be storing hashed pw
       t.string :firstName
       t.string :lastName
+      t.boolean :isExaminer
+      t.text :bio
+      t.integer :rates
 
       t.timestamps
     end
