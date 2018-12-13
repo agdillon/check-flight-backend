b2f77582300aefd01d0d29211abee4e4439338be
Amanda Dillon <agdillon@gmail.com>
2018-12-13 10:24:15 -0700

    working users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
-  resources :examiners
+  resources :users do
+    resources :avails
+  end
+  #resources :airports
 end
