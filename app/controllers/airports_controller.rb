6196798fb00de745cb540b3469c7a5c3b8b3e4ce
Amanda Dillon <agdillon@gmail.com>
2018-12-16 16:21:16 -0700

    nest examiners under airports

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -2,12 +2,14 @@ class AirportsController < ApplicationController
   # GET /airports
   def index
     @airports = Airport.all
-    json_response(@airports)
+    # json_response(@airports)
+    render json: @airports.as_json(include: [:users]), status: status
   end
 
   # GET /airports/:id
   def show
     @airport = Airport.find(params[:id])
-    json_response(@airport)
+    # json_response(@airport)
+    render json: @airport.as_json(include: [:users]), status: status
   end
 end
